18e1c3309bfbc900a0bcfd1222a86196d81bc39c
Will Grathwohl <willgrathwohl@WillsMacBookPro.local>
2017-08-23 19:21:15 -0400

    batch

--- gan_mnist.py
+++ gan_mnist.py
@@ -246,7 +246,7 @@ elif MODE == 'dcgan':
 fixed_noise = tf.constant(np.random.normal(size=(128, 128)).astype('float32'))
 fixed_noise_samples = Generator(128, noise=fixed_noise)
 def generate_image(frame, true_dist):
-    samples = session.run(fixed_noise_samples)
+    samples = session.run(fixed_noise_samples, feed_dict={real_data: true_dist})
     lib.save_images.save_images(
         samples.reshape((128, 28, 28)), 
         '{}/samples_{}.png'.format(TRAIN_DIR, frame)
